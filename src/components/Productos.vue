a88416e175770558c57819a6a220583020aeed28
Luis Joaquín Caroca Araya <lj.caroca@gmail.com>
2025-05-29 10:30:48 -0400

    Se agrego nombre y precio de los productos

--- src/components/Productos.vue
+++ src/components/Productos.vue
@@ -18,7 +18,8 @@
             alt="..."
           />
           <div class="card-body">
-            <h5 class="card-title fw-bold">placeholder</h5>
+            <h5 class="card-title fw-bold">Tazón 11 oz</h5>
+            <h6 class="card-title fw-bold">$4.000</h6>
             <p class="card-text">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam
               facere magnam rerum quidem nam esse architecto adipisci
@@ -46,7 +47,8 @@
             alt="..."
           />
           <div class="card-body">
-            <h5 class="card-title fw-bold">placeholder</h5>
+            <h5 class="card-title fw-bold">Botella deportiva</h5>
+            <h6 class="card-title fw-bold">$7.000</h6>
             <p class="card-text">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
               minus tempore necessitatibus praesentium cumque similique dolores
@@ -56,7 +58,65 @@
             <a
               href="#"
               class="btn btn-primary mt-auto text-white"
-              >Ingresar al sitio</a
+              >Ver producto</a
+            >
+          </div>
+        </div>
+      </div>
+      <!-- Fin de una tarjeta -->
+      <!-- Inicio de una tarjeta -->
+      <div class="col">
+        <div
+          class="card h-100 mx-auto"
+          style="width: 25rem"
+        >
+          <img
+            src="../assets/productos_img/placeholder.png"
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body">
+            <h5 class="card-title fw-bold">Mouse pad</h5>
+            <h6 class="card-title fw-bold">$5.000</h6>
+            <p class="card-text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam
+              facere magnam rerum quidem nam esse architecto adipisci
+              repudiandae vitae dignissimos. Dolorum iste dignissimos rerum
+              laboriosam, aliquam facere omnis magni quis.
+            </p>
+            <a
+              href="#"
+              class="btn btn-primary mt-auto text-white"
+              >Ver producto</a
+            >
+          </div>
+        </div>
+      </div>
+      <!-- Fin de una tarjeta -->
+      <!-- Inicio de una tarjeta -->
+      <div class="col">
+        <div
+          class="card h-100 mx-auto"
+          style="width: 25rem"
+        >
+          <img
+            src="../assets/productos_img/placeholder.png"
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body">
+            <h5 class="card-title fw-bold">Foto roca</h5>
+            <h6 class="card-title fw-bold">$10.000</h6>
+            <p class="card-text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam
+              facere magnam rerum quidem nam esse architecto adipisci
+              repudiandae vitae dignissimos. Dolorum iste dignissimos rerum
+              laboriosam, aliquam facere omnis magni quis.
+            </p>
+            <a
+              href="#"
+              class="btn btn-primary mt-auto text-white"
+              >Ver producto</a
             >
           </div>
         </div>
