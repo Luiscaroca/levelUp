c3a82931ca70e1f771fa50e894e0e80828e0a20d
Luis Joaquín Caroca Araya <lj.caroca@gmail.com>
2025-05-29 09:48:17 -0400

    Se modifico grilla de productos

--- src/components/Productos.vue
+++ src/components/Productos.vue
@@ -5,108 +5,58 @@
     data-aos-duration="500"
   >
     <h3 class="mt-5 text-center">Productos</h3>
-    <div class="mb-5 row row-cols-1 row-cols-md-3 g-4 w-100">
+    <div class="mt-4 mb-5 row row-cols-1 row-cols-md-3 g-4">
       <!-- Inicio de una tarjeta -->
       <div class="col">
         <div
           class="card h-100 mx-auto"
-          style="width: 20rem"
+          style="width: 25rem"
         >
           <img
             src="../assets/productos_img/placeholder.png"
             class="card-img-top"
             alt="..."
           />
-          <div class="card-body d-flex flex-column">
-            <h5 class="card-title">Card title</h5>
+          <div class="card-body">
+            <h5 class="card-title fw-bold">placeholder</h5>
             <p class="card-text">
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam
+              facere magnam rerum quidem nam esse architecto adipisci
+              repudiandae vitae dignissimos. Dolorum iste dignissimos rerum
+              laboriosam, aliquam facere omnis magni quis.
             </p>
             <a
               href="#"
-              class="btn btn-primary mt-auto"
+              class="btn btn-primary mt-auto text-white"
               >Ver producto</a
             >
           </div>
         </div>
       </div>
       <!-- Fin de una tarjeta -->
-
       <!-- Inicio de una tarjeta -->
       <div class="col">
         <div
           class="card h-100 mx-auto"
-          style="width: 20rem"
+          style="width: 25rem"
         >
           <img
             src="../assets/productos_img/placeholder.png"
             class="card-img-top"
             alt="..."
           />
-          <div class="card-body d-flex flex-column">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a short card.</p>
-            <a
-              href="#"
-              class="btn btn-primary mt-auto"
-              >Ver producto</a
-            >
-          </div>
-        </div>
-      </div>
-      <!-- Fin de una tarjeta -->
-
-      <!-- Inicio de una tarjeta -->
-      <div class="col">
-        <div
-          class="card h-100 mx-auto"
-          style="width: 20rem"
-        >
-          <img
-            src="../assets/productos_img/placeholder.png"
-            class="card-img-top"
-            alt="..."
-          />
-          <div class="card-body d-flex flex-column">
-            <h5 class="card-title">Card title</h5>
+          <div class="card-body">
+            <h5 class="card-title fw-bold">placeholder</h5>
             <p class="card-text">
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+              minus tempore necessitatibus praesentium cumque similique dolores
+              eum quasi qui veniam, et culpa? Odio facere adipisci sit
+              perspiciatis quam nihil magni!
             </p>
             <a
               href="#"
-              class="btn btn-primary mt-auto"
-              >Ver producto</a
-            >
-          </div>
-        </div>
-      </div>
-      <!-- Fin de una tarjeta -->
-
-      <!-- Inicio de una tarjeta -->
-      <div class="col">
-        <div
-          class="card h-100 mx-auto"
-          style="width: 20rem"
-        >
-          <img
-            src="../assets/productos_img/placeholder.png"
-            class="card-img-top"
-            alt="..."
-          />
-          <div class="card-body d-flex flex-column">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
-            <a
-              href="#"
-              class="btn btn-primary mt-auto"
-              >Ver producto</a
+              class="btn btn-primary mt-auto text-white"
+              >Ingresar al sitio</a
             >
           </div>
         </div>
